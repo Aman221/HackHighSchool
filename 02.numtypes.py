f0d06c05a30edd2bfd17cf6082a666b4a8af64ae
Samyak Ghevaria <theshammy123@gmail.com>
2018-10-20 14:08:43 -0700

    Hacker age stuff
    Done

---
+++ 02.numtypes.py
@@ -0,0 +1,5 @@
+print "Hello Hacker, what is your age?"
+response = raw_input('Type your age here: ')
+print "In 5 years, you will be " + str(int(response) + 5)
+print "When you live ten times as long you will be " + str(int(response) * 10)
+print "Your age divided by 3 equals " + str(int(response) / 3) + " remainder " + str(int(response) % 3)
