c13f4b2993f4ccdcf6805cbb16e156ade067ac63
Samyak Ghevaria <theshammy123@gmail.com>
2018-10-20 13:51:40 -0700

    Mad Libs
    Done

---
+++ 00_so_long.py
@@ -0,0 +1,33 @@
+animal = "hippos"
+emotion = "angry"
+trait = "fragileness"
+adjective = "feeble"
+
+print "So long and thanks for all the " + str(animal) 
+print "So " + str(emotion) + " that it should come to this." 
+print "We tried to warn you all but oh dear?" 
+print " "
+print "You may not share our " + str(trait) 
+print "Which might explain your disrespect"
+print "For all the " + str(adjective) + " wonders that"
+print "grow around you"
+print " "
+print "So long, so long, and thanks"
+print "for all the " + str(animal)
+print " "
+animal_2 = "alligators"
+print " "
+print "The world is about to be destroyed"
+print "There is no point getting all " + str(emotion)
+print "Lie back and let the planet dissolve"
+print " "
+print "Despite those nets of " + str(animal_2) + " fleet"
+print "We thought that most of you were " + str(emotion)
+print "Especially " + str(adjective) + " tots and your"
+print "pregnant women"
+print " "
+print "So long, so long, so long, so long, so long"
+print "So long, so long, so long, so long, so long"
+print " "
+print "So long, so long, and thanks"
+print "for all the " + str(animal)
