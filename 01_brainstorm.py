fcedf7bcc96c5c50583650dee166c6280d8b15db
Amanuel <amanu.ayalew@gmail.com>
2018-10-29 06:19:49 -0700

    Now with a random number generator

---
+++ 01_brainstorm.py
@@ -0,0 +1,11 @@
+print "Welcome to Brainstorm, everyone's favorite ghetto version of Scattegories!"
+Actors_and_Directors = 1
+World_Nations = 2
+International_Cuisine = 3
+Futbol = 4
+Culture = 5
+
+
+import random
+for x in range(1):
+	print random.randint(1,5)
