1adfa74e623004b1dd1b19692f602388b09bdc56
Amanuel <amanu.ayalew@gmail.com>
2018-11-03 13:25:27 -0700

    YEEE

--- 01_brainstorm.py
+++ 01_brainstorm.py
@@ -1,11 +1,52 @@
 print "Welcome to Brainstorm, everyone's favorite ghetto version of Scattegories!"
-Actors_and_Directors = 1
-World_Nations = 2
-International_Cuisine = 3
-Futbol = 4
-Culture = 5
 
+category1 = "Name a food: "
+category2 = "Name a country: "
+category3 = "Name a company: "
+categorylist = [category1, category2, category3]
+answerlist = 
 
 import random
 for x in range(1):
-	print random.randint(1,5)
+	category = random.randint(0,2)
+
+if category == 0:
+		response_a1 = raw_input(categorylist[0])
+		answerlist.append(response_a1)
+		response_a2 = raw_input(categorylist[0])
+		answerlist.append(response_a2)
+		response_a3 = raw_input(categorylist[0])
+		answerlist.append(response_a3)
+		response_a4 = raw_input(categorylist[0])
+		answerlist.append(response_a4)
+		response_a5 = raw_input(categorylist[0])
+		answerlist.append(response_a5)
+		response_a6 = raw_input(categorylist[0])
+		answerlist.append(response_a6)
+		response_a7 = raw_input(categorylist[0])
+		answerlist.app
+		response_a8 = raw_input(categorylist[0])
+		response_a9 = raw_input(categorylist[0])
+		response_a10 = raw_input(categorylist[0])
+elif category == 1:
+		response_b1 = raw_input(categorylist[1])
+		response_b2 = raw_input(categorylist[1])
+		response_b3 = raw_input(categorylist[1])
+		response_b4 = raw_input(categorylist[1])
+		response_b5 = raw_input(categorylist[1])
+		response_b6 = raw_input(categorylist[1])
+		response_b7 = raw_input(categorylist[1])
+		response_b8 = raw_input(categorylist[1])
+		response_b9 = raw_input(categorylist[1])
+		response_b10 = raw_input(categorylist[1])
+elif category == 2:
+		response_c1 = raw_input(categorylist[2])
+		response_c2 = raw_input(categorylist[2])
+		response_c3 = raw_input(categorylist[2])
+		response_c4 = raw_input(categorylist[2])
+		response_c5 = raw_input(categorylist[2])
+		response_c6 = raw_input(categorylist[2])
+		response_c7 = raw_input(categorylist[2])
+		response_c8 = raw_input(categorylist[2])
+		response_c9 = raw_input(categorylist[2])
+		response_c10 = raw_input(categorylist[2])
