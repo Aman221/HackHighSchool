8512955a4f70bdd9b50566c769ffdcf07919463f
Amanuel <amanu.ayalew@gmail.com>
2018-11-10 14:15:43 -0800

    Update 02_bool.py

--- 02_bool.py
+++ 02_bool.py
@@ -1,1 +1,4 @@
-eckis de
+list1 = [False,True,True,None,True,None,None,False,False,None,True,False]
+list2 = ["or","or","or","==","!=","==","and","==","!=","and","==","or"]
+list3 = [False,False,None,None,True,True,False,True,None,False,True,None]
+list4 - []
