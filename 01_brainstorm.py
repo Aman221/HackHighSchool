b0ddf7c10761e69486326d4019501b3bf0b986b2
Amanuel <amanu.ayalew@gmail.com>
2018-11-10 12:16:45 -0800

    Update 01_brainstorm.py

--- 01_brainstorm.py
+++ 01_brainstorm.py
@@ -19,9 +19,7 @@ print "Your category is: " + category
 import time
 
 start = time.time()
-end = time.time()
 new_start = int(start)
-new_end = int(end)
 
 
 response1 = raw_input(category + ':')
@@ -45,10 +43,12 @@ anslist.append(response9)
 print "What is your last item?"
 response10 = raw_input(category + ':')
 anslist.append(response10)
+end = time.time()
+new_end = int(end)
 Overall_time = new_end-new_start
 new_overall_time = str(Overall_time)
 
 for x in anslist:
-	print(x.center(80,' '))
-	print (new_overall_time.center(80,' '))
-
+	print(x.center(100,' '))
+Elapsed_time =  "You took " + new_overall_time + " seconds"
+print(Elapsed_time.center(80,' '))
