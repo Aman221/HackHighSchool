2a10db3907334f62d22ce75cd3bd744b0f819586
Samyak Ghevaria <theshammy123@gmail.com>
2018-11-17 12:23:00 -0800

    Updated

--- 02_bool.py
+++ 02_bool.py
@@ -1,6 +1,5 @@
 list1 = [False,True,True,None,True,None,None,False,False,None,True,False]
 list2 = ["or","or","or","==","!=","==","and","==","!=","and","==","or"]
 list3 = [False,False,None,None,True,True,False,True,None,False,True,None]
-list4 = []
 
-for 
+bool ( list1[0] + list2[0] + list3[0] )
