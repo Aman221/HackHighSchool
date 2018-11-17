38f4a8551cd2cd6cdc9f8734b47781bcec362131
Samyak Ghevaria <theshammy123@gmail.com>
2018-11-17 12:24:29 -0800

    new

---
+++ 02_Bool.py
@@ -0,0 +1,5 @@
+list1 = [False,True,True,None,True,None,None,False,False,None,True,False]
+list2 = ["or","or","or","==","!=","==","and","==","!=","and","==","or"]
+list3 = [False,False,None,None,True,True,False,True,None,False,True,None]
+
+print (str((bool(list1[0]))) + " " + str(list2[0]) + " " + str((bool(list3[0]))))
