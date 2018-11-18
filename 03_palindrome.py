7f2661013f81d061c2f8cf0820e3d25bb7cdcfb4
Amanuel <amanu.ayalew@gmail.com>
2018-11-18 09:55:42 -0800

    Palindrome Perfection

--- 03_palindrome.py
+++ 03_palindrome.py
@@ -3,7 +3,9 @@ print "By royal decree, her highness Queen Hannah has declared that only palindr
 print "I am the royal guard, Noon!"
 print "What is thy name? For that will decide whether or not thou may enter these sacred walls!"
 palindrometest = raw_input('-')
-if palindrometest == palindrometest[::-1]:
+letters = ''.join(filter(str.isalpha, palindrometest))
+lower = letters.lower()
+if lower == lower[::-1]:
 	print "Welcome to Palindrome Palace, palindrome! Thou art welcome now and forevermore!"
 else:
 	print "Apologies, non-palindrome! Thou art not welcome in these walls! No exceptions! Perhaps thou should try the Realm of Rhymes next door."
