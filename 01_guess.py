4497ed2d1c9e89350f4929bc239ce7d701455f4c
Amanuel <amanu.ayalew@gmail.com>
2018-11-22 07:19:12 -0800

    Update 01_guess.py
    Bug fixes and replaced randomly generated word with user guess

--- 01_guess.py
+++ 01_guess.py
@@ -6,30 +6,51 @@ word = random.randint(1,6)
 
 if word == 1:
 	cat = garray[0]
-	print "The first letter of the word is 'd'. Start guessing!"
+	ultimate_first = cat[:1]
+	print "The first letter of the word is " + ultimate_first + " . Start guessing!"
 if word == 2:
 	cat = garray[1]
-	print "The first letter of the word is 'f'. Start guessing!"
+	ultimate_first = cat[:1]
+	print "The first letter of the word is " + ultimate_first + " . Start guessing!"
 if word == 3:
 	cat = garray[2]
-	print "The first letter of the word is 'l'. Start guessing!"
+	ultimate_first = cat[:1]
+	print "The first letter of the word is " + ultimate_first + " . Start guessing!"
 if word == 4:
 	cat = garray[3]
-	print "The first letter of the word is 'w'. Start guessing!"
+	ultimate_first = cat[:1]
+	print "The first letter of the word is " + ultimate_first + " . Start guessing!"
 if word == 5:
 	cat = garray[4]
-	print "The first letter of the word is 'a'. Start guessing!"
+	ultimate_first = cat[:1]
+	print "The first letter of the word is " + ultimate_first + " . Start guessing!"
 if word == 6:
 	cat = garray[5]
-	print "The first letter of the word is 's'. Start guessing!"
-
+	ultimate_first = cat[:1]
+	print "The first letter of the word is " + ultimate_first + " . Start guessing!"
 
 i = 1
 while i < 11:
-	
-	i = i + 1
-	if i == 11:
+	user_guess = raw_input('-')
+	if user_guess == '':
+		print "You wasted a guess =P"
+		i = i + 1
+	elif user_guess == cat:
+		print "Good Job! You are one with the Source."
 		break
-
-
-print "You lose, better luck next time!"
+	elif len(user_guess) != 5:
+		print "0, 1, 2, 3, 4, that's how we count to 5!"
+		i = i + 1
+	elif ultimate_first != user_guess[:1] and len(user_guess) == (5):
+		print "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+		i = i
+	elif ultimate_first == user_guess[:1] and len(user_guess) == (5):
+		if i == 9:
+			print "you have " + str((10 - i)) + " guess left."
+			i = i + 1
+		else:
+			print "you have " + str((10 - i)) + " guesses left."
+			i = i + 1
+	if i == 11:
+		print "You lose, better luck next time!"
+		break
