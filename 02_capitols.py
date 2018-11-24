b66185ddf41b718c292dfb64ecda0053f686e3c4
Amanuel <amanu.ayalew@gmail.com>
2018-11-23 21:27:33 -0800

    Update 02_capitols.py

--- 02_capitols.py
+++ 02_capitols.py
@@ -1,19 +1,19 @@
 while True:
+	import csv
 	user_input = raw_input('Ready: ')
-	dictionary = {}
-	with open("capitols.txt") as fh:
-		for line in fh:
-			key, val = line.strip().split(',')
-			dictionary[key.strip()] = val.strip()
-			print dictionary.items()
-			if user_input in dictionary.keys():
-				print val
-				break
-			elif user_input in dictionary.values():
-				print key
-				break
-			elif user_input ==  "Done":
-				quit()
-			else:
-				print "nil"
-				break
+	fh = csv.reader(open("capitols.txt", "r"))
+	dictionary = {}	
+	for line in fh:
+		key, val = line
+		dictionary[key] = val
+		if user_input in dictionary.keys():
+			print val
+			break
+		elif user_input in dictionary.values():
+			print key
+			break
+		elif user_input ==  "Done":
+			quit()
+		else:
+			print "nil"
+			break
