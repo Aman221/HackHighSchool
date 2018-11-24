941026f6708386a2cc79523947684797aae5e0a4
Amanuel <amanu.ayalew@gmail.com>
2018-11-23 16:26:47 -0800

    SO CLOSE BEEN AT IT FOR THE BETTER PART OF 15 HOURS
    I'm not even kidding. Please don't touch this.
    
    It's personal. >:-(
    *cracks knuckles menacingly*
    *scowls*

--- 02_capitols.py
+++ 02_capitols.py
@@ -3,12 +3,13 @@ while True:
 	dictionary = {}
 	with open("capitols.txt") as fh:
 		for line in fh:
-			key, val = line.split(',')
-			dictionary[str(key)] = val
-			if user_input in key or user_input in key.lower():
+			key, val = line.strip().split(',')
+			dictionary[key.strip()] = val.strip()
+			print dictionary.items()
+			if user_input in dictionary.keys():
 				print val
 				break
-			elif user_input in val or user_input in val.lower():
+			elif user_input in dictionary.values():
 				print key
 				break
 			elif user_input ==  "Done":
