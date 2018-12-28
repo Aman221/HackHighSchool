2a4cd4df5181eeaeed87744325c4e2440eb56d5e
Amanuel <amanu.ayalew@gmail.com>
2018-12-28 08:03:15 -0800

    The end of the beginning of the end!
    :)

--- Parseltongue Piscine Part 4/00_rainbow.py
+++ Parseltongue Piscine Part 4/00_rainbow.py
@@ -1,14 +1,46 @@
-def print_format_table():
-	"""
-	prints table of formatted text format options
-	"""
-	for style in range(8):
-		for foreground_color in range(30,38):
-			s1 = ''
-			for background_color in range(40,48):
-				format = ';'.join([str(style), str(foreground_color), str(background_color)])
-				s1 += '\x1b[%sm %s \x1b[0m' % (format, format)
-			print(s1)
-		print('\n')
+i = 0
 
-print_format_table()
+Black = 30
+Red = 31
+Green = 32
+Yellow = 33
+Blue = 34
+Purple = 35
+Cyan = 36
+White = 37
+
+Default = 0
+Bold = 1
+Faded = 2
+Italics = 3
+Underlined = 4
+Blinking = 5
+TT = 6
+Negative = 7
+
+BG_Black = 40
+BG_Red = 41
+BG_Green = 42
+BG_Yellow = 43
+BG_Blue = 44
+BG_Purple = 45
+BG_Cyan = 46
+BG_White = 47
+
+def write_colorful_text(string, style, foreground_color, background_color):
+	if style in range(8):
+		if foreground_color in range(30,38):
+			if background_color in range(40,48):
+				while i < len(string):
+					print "\033[" + str(style) + ";" + str(foreground_color) + ";" + str(background_color) + "m " + string[i] + " \n"
+					global i
+					i = i + 1
+					return
+
+write_colorful_text("RAINBOW", Blinking, Red, BG_White)
+write_colorful_text("RAINBOW", Blinking, Yellow, BG_White)
+write_colorful_text("RAINBOW", Blinking, Green, BG_White)
+write_colorful_text("RAINBOW", Blinking, Cyan, BG_White)
+write_colorful_text("RAINBOW", Blinking, Blue, BG_White)
+write_colorful_text("RAINBOW", Blinking, Purple, BG_White)
+write_colorful_text("RAINBOW", Blinking, Black, BG_White)
