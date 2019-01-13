e974a96be491505b8d5f9dfc6aaadf8f70a40021
Amanuel <amanu.ayalew@gmail.com>
2019-01-12 16:14:44 -0800

    BREAKTHROUGH

--- Parseltongue Piscine Part 4/02_phonebook.py
+++ Parseltongue Piscine Part 4/02_phonebook.py
@@ -13,7 +13,12 @@ for line in f.readlines():
 	b = "".join(a)
 	lov.append(b)
 c = collections.Counter(lok)
-d = [k for k, v in c.iteritems() if v > 1]
+d = {k:str(v) for k, v in c.iteritems()}
+pk = {v:[k for k in c if c[k] == v] for v in c.itervalues()}
+del pk[1] 
+print pk
 y = collections.Counter(lov)
-z = [k for k, v in y.iteritems() if v > 1]
-
+z = {k:str(v) for k, v in y.iteritems()}
+pv = {v:[k for k in y if y[k] == v] for v in y.itervalues()}
+del pv[1]
+print pv
