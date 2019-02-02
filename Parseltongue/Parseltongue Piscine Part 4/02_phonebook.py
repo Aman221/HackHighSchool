84321ccf07303e594c26de9d4be53932c8dfbf57
Samyak Ghevaria <theshammy123@gmail.com>
2019-02-02 15:45:56 -0800

    Merge branch 'master' of https://github.com/Aman221/HackHighSchool

--- Parseltongue/Parseltongue Piscine Part 4/02_phonebook.py
+++ Parseltongue/Parseltongue Piscine Part 4/02_phonebook.py
@@ -16,4 +16,10 @@ c = collections.Counter(lok)
 d = [k for k, v in c.iteritems() if v > 1]
 y = collections.Counter(lov)
 z = [k for k, v in y.iteritems() if v > 1]
-
+print "Shared First Names!"
+for x in d:
+	print x + "(" + str(lok.count(x)) + ")" + ": " + str([v for k, v in dictionary.iteritems() if k == x])
+print "\n"
+print "Shared Last Names!"
+for x in z:
+	print x + "(" + str(lov.count(x)) + ")" + ": " + str([k for k, v in dictionary.iteritems() if v == x])
