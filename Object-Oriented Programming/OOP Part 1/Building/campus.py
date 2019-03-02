debae17298ac003d7cdc08e8e20e0f544b2591ea
Amanuel <amanu.ayalew@gmail.com>
2019-03-02 12:31:52 -0800

    updates

--- Object-Oriented Programming/OOP Part 1/Building/campus.py
+++ Object-Oriented Programming/OOP Part 1/Building/campus.py
@@ -2,10 +2,16 @@ from building import Building
 
 class Campus:
 	def __init__ (self, buildings, num_buildings, capacity):
+		self.buildings = buildings
+		self.num_buildings = number_of_buildings
+		self.capacity = ccapacity
+
 		def add_building():
-			self.buildings = []
-			self.num_buildings = len(self.buildings)
-			self.capacity = num
+			buildings = []
+			number_of_buildings = len(buildings)
+			ccapacity = capacity
 
 		def get_info():
+			return "The campus has" + len(buildings) + "with a combined capacity of "
+
 			
