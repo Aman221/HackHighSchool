e806bd5400e65e9458ca1353188e852be9f82244
Amanuel <amanu.ayalew@gmail.com>
2019-03-09 14:14:00 -0800

    FINITO

---
+++ Object-Oriented Programming/OOP Part 1/Jokes/joke.py
@@ -0,0 +1,4 @@
+class Joke:
+	def reply(self):
+		arr = [73, 110, 104, 101, 114, 105, 116, 97, 110, 99, 101]
+		print(''.join([chr(n) for n in arr]))
---
+++ Object-Oriented Programming/OOP Part 1/Jokes/main.py
@@ -0,0 +1,12 @@
+from dad import Dad
+from joke import Joke
+
+def main():
+	d = Dad()
+	d.speak()
+	j = Joke()
+	j.reply()
+
+
+if __name__ == '__main__':
+	main()
